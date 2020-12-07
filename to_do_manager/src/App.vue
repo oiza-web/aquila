aebd40c5acc29ac8a5e07c3b198a27659634d4f5
Mary <maryolorunfemi6@gmail.com>
2020-12-07 14:25:16 +0100

    Todo List

---
+++ to_do_manager/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <AddTodo/>
+      <FilterTodos/>
+      <Todos/>
+    </div>
+  
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos.vue'
+import AddTodo from './components/AddTodo.vue'
+import FilterTodos from './components/FilterTodos.vue';
+
+export default {
+  name: 'App',
+  components: {
+      Todos,
+      AddTodo,
+      FilterTodos
+  }
+}
+</script>
+
+
+<style scoped>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
+  cursor: pointer;
+}
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+.is-complete {
+  background: #35495e;
+  color: #fff;
+}
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
